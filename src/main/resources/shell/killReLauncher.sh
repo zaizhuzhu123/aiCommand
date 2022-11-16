f8288442144281a4356980c277cdac523edea031
laienQmf <651857512@qq.com>
2022-11-16 19:57:42 +0800

    更新包

--- src/main/resources/shell/killReLauncher.sh
+++ src/main/resources/shell/killReLauncher.sh
@@ -1,4 +1,4 @@
 pid=`ps -aux | grep launcher | awk '{print $2}' `
-if [ $pid ]; then
+if test $pid
 kill -9 $pid
 fi
--- src/main/resources/shell/killWebui.sh
+++ src/main/resources/shell/killWebui.sh
@@ -1,4 +1,4 @@
 pid=`ps -aux | grep webui | awk '{print $2}' `
 if test $pid
 kill -9 $pid
-end
+fi
