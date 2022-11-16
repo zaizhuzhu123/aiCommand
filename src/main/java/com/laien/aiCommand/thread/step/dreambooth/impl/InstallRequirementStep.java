009465faebe1bc72b88e32b6e8a08a2a1ca53f18
laienQmf <651857512@qq.com>
2022-11-16 20:36:02 +0800

    更新包

--- src/main/java/com/laien/aiCommand/thread/step/dreambooth/impl/InstallRequirementStep.java
+++ src/main/java/com/laien/aiCommand/thread/step/dreambooth/impl/InstallRequirementStep.java
@@ -1,28 +1,28 @@
-//package com.laien.aiCommand.thread.step.dreambooth.impl;
-//
-//import com.laien.aiCommand.schedule.impl.process.util.CommandExecutor;
-//import com.laien.aiCommand.thread.step.dreambooth.InstallDreamBoothStep;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.core.annotation.Order;
-//import org.springframework.stereotype.Component;
-//
-//import javax.annotation.Resource;
-//import java.io.IOException;
-//import java.util.concurrent.TimeUnit;
-//
-//@Component
-//@Order(2)
-//@Slf4j
-//public class InstallRequirementStep implements InstallDreamBoothStep {
-//
-//    @Resource
-//    private CommandExecutor commandExecutor;
-//
-//    @Override
-//    public void run() throws IOException, InterruptedException {
-//        log.info("-------------------------------------------");
-//        log.info(this.getClass().getSimpleName());
-//        String cmd = "sh /workspace/aiCommand/target/soft/shell/installRequirement.sh";
-//        commandExecutor.execResult(300, TimeUnit.SECONDS, cmd);
-//    }
-//}
+package com.laien.aiCommand.thread.step.dreambooth.impl;
+
+import com.laien.aiCommand.schedule.impl.process.util.CommandExecutor;
+import com.laien.aiCommand.thread.step.dreambooth.InstallDreamBoothStep;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@Order(2)
+@Slf4j
+public class InstallRequirementStep implements InstallDreamBoothStep {
+
+    @Resource
+    private CommandExecutor commandExecutor;
+
+    @Override
+    public void run() throws IOException, InterruptedException {
+        log.info("-------------------------------------------");
+        log.info(this.getClass().getSimpleName());
+        String cmd = "sh /workspace/aiCommand/target/soft/shell/installRequirement.sh";
+        commandExecutor.execResult(300, TimeUnit.SECONDS, cmd);
+    }
+}
